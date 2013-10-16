3ed1ddb0ac8dda67b02e661d0f5adfe9a056a5eb
rburgess <rburgess@evernote.com>
2013-10-15 23:12:08 -0700

    clean up  javascript

--- js/modals.js
+++ js/modals.js
@@ -10,50 +10,43 @@
         //load mask
         $("<div id='mask'></div>").appendTo("body");
 
-        //remove existing modal window
-        $("#modal-window").remove();
-
         var content = $(this).attr("href");
-        var fullContent = content;
 
         //if youtube video or images passed into modal
-        if(content.indexOf("youtube.com/embed") !== -1){
-            fullContent = "<iframe src='"+ content +"' allowfullscreen='allowfullscreen'></iframe>";
-        }else if(content.indexOf("jpg") !== -1 || content.indexOf("png") !== -1){
-            fullContent = "<img src='"+ content +"'>";
+        if (content.indexOf("youtube.com/embed") !== -1) {
+            content = "<iframe src='" + content + "' allowfullscreen='allowfullscreen'></iframe>";
+        } else if (content.indexOf("jpg") !== -1 || content.indexOf("png") !== -1) {
+            content = "<img src='" + content + "'>";
         }
 
         //load modal window with content passed
-        $("<div id='modal-window' class='window'>"+ fullContent +"<a href='#' class='close'>X</a></div>").appendTo("body");
+        $("<div id='modal-window' class='window'>" + content + "<a href='#' class='close'>X</a></div>").appendTo("body");
 
         var maskHeight = $(document).height();
         var maskWidth = $(window).width();
 
-        $("#mask").css({"width":maskWidth,"height":maskHeight});
+        $("#mask").css({"width": maskWidth, "height": maskHeight})
+                  .show();
 
-        $("#mask").show();
-
-        $(".window").css("top", ( $(window).height() - $(".window").height() ) / 2+$(window).scrollTop() + "px");
-        $(".window").css("left", ( $(window).width() - $(".window").width() ) / 2+$(window).scrollLeft() + "px");
-
-        $(".window").fadeIn();
+        $(".window").css("top", ($(window).height() - $(".window").height()) / 2 + $(window).scrollTop() + "px")
+                    .css("left", ($(window).width() - $(".window").width()) / 2 + $(window).scrollLeft() + "px")
+                    .fadeIn();
 
         e.preventDefault();
         return false;
     });
 
-    function removeItems() {
+    var removeItems = function () {
+        $("#mask, .window").fadeOut();
+        $(".window iframe").remove();
         setTimeout(function () {
             $("#mask, .window").remove();
         }, 500);
-    }
-    
+    };
     //if close button is clicked
     $("body").on("click", ".window .close", function (e) {
         //Cancel the link behavior
         e.preventDefault();
-        $("#mask, .window").fadeOut();
-        $(".window iframe").remove();
         removeItems();
     });
 
@@ -61,20 +54,17 @@
     $("body").on("click", "#mask", function (e) {
         //Cancel the link behavior
         e.preventDefault();
-        $("#mask, .window").hide();
-        $(".window iframe").remove();
         removeItems();
     });
 
     // Function to check the current window size for responsive pages
     // Change modal window size repsonsive
     function checkWindowSize() {
-        $(".window").css("top", ( $(window).height() - $(".window").height() ) / 2+$(window).scrollTop() + "px");
-        $(".window").css("left", ( $(window).width() - $(".window").width() ) / 2+$(window).scrollLeft() + "px");
-        $("#mask").css({"width":$(window).width(),"height":$(document).height()});
+        $(".window").css("top", ($(window).height() - $(".window").height()) / 2 + $(window).scrollTop() + "px")
+                    .css("left", ($(window).width() - $(".window").width()) / 2 + $(window).scrollLeft() + "px");
+        $("#mask").css({"width":    $(window).width(), "height": $(document).height()});
     }
     $(window).bind("resize", checkWindowSize);
-    
-    
+
 }(jQuery));
 
