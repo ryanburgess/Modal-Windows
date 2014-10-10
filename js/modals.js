c6a03ec8c37519df6870898834bdf81a479ea70d
Ryan <rburgess@evernote.com>
2014-10-10 10:01:35 -0700

    clean up js

--- js/modals.js
+++ js/modals.js
@@ -2,73 +2,79 @@
 //Modal Windows Script
 
 (function ($) {
-    "use strict";
-
-    //modal windows
-    $(".modal").on("click", function (e) {
-
-        //load mask
-        $("<div id='mask'></div>").appendTo("body");
-
-        var content = $(this).attr("href");
-        var vidParameters = '?vq=hd720&autoplay=1&showinfo=0&rel=0';
-        //if youtube video or images passed into modal
-        if (content.indexOf("youtube.com/embed") !== -1) {
-            content = "<iframe src='" + content + vidParameters +"' allowfullscreen='allowfullscreen'></iframe>";
-        }else if(content.indexOf('vimeo.com/') !== -1){
-            var vidId = content.split('vimeo.com/');
-            vidId = vidId[1];
-            fullContent = '<iframe src="//player.vimeo.com/video/'+ vidId +'?color=9C191E&amp;autoplay=1" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-        }else if (content.indexOf("jpg") !== -1 || content.indexOf("png") !== -1) {
-            content = "<img src='" + content + "'>";
-        }
-
-        //load modal window with content passed
-        $("<div id='modal-window' class='window'>" + content + "<a href='#' class='close'>X</a></div>").appendTo("body");
-
-        var maskHeight = $(document).height();
-        var maskWidth = $(window).width();
-
-        $("#mask").css({"width": maskWidth, "height": maskHeight})
-                  .show();
-
-        $(".window").css("top", ($(window).height() - $(".window").height()) / 2 + $(window).scrollTop() + "px")
-                    .css("left", ($(window).width() - $(".window").width()) / 2 + $(window).scrollLeft() + "px")
-                    .fadeIn();
-
-        e.preventDefault();
-        return false;
-    });
-
-    var removeItems = function () {
-        $("#mask, .window").fadeOut();
-        $(".window iframe").remove();
-        setTimeout(function () {
-            $("#mask, .window").remove();
-        }, 500);
-    };
-    //if close button is clicked
-    $("body").on("click", ".window .close", function (e) {
-        //Cancel the link behavior
-        e.preventDefault();
-        removeItems();
-    });
-
-    //if mask is clicked
-    $("body").on("click", "#mask", function (e) {
-        //Cancel the link behavior
-        e.preventDefault();
-        removeItems();
-    });
-
-    // Function to check the current window size for responsive pages
-    // Change modal window size repsonsive
-    function checkWindowSize() {
-        $(".window").css("top", ($(window).height() - $(".window").height()) / 2 + $(window).scrollTop() + "px")
-                    .css("left", ($(window).width() - $(".window").width()) / 2 + $(window).scrollLeft() + "px");
-        $("#mask").css({"width":    $(window).width(), "height": $(document).height()});
+  'use strict';
+  //load modal mask
+  $('<div id="mask"></div>').appendTo('body');
+  //modal windows
+  $('.modal').on('click', function (e) {
+    //remove existing modal window
+    $('#modal-window').remove();
+    var content,
+      maskHeight = $(document).height(),
+      maskWidth = $(window).width();
+    //Checks if href points to media or a div that is defined in the DOM
+    if ($(this).attr('href').match('^#')) {
+      var contentID = $(this).attr('href');
+      content = $(contentID).html();
+    } else {
+      content = $(this).attr('href');
     }
-    $(window).bind("resize", checkWindowSize);
-
+    var fullContent = content,
+      vidParameters = '?vq=hd720&autoplay=1&showinfo=0&rel=0';
+    //if youtube video or images passed into modal
+    if (content.indexOf('youtube.com/embed/') !== -1) {
+      fullContent = '<iframe src="' + content + vidParameters + '" allowfullscreen="allowfullscreen"></iframe>';
+    } else if (content.indexOf('jpg') !== -1 || content.indexOf('png') !== -1) {
+      fullContent = '<img src="' + content + '">';
+    }
+    //support youku videos for YXBJ, added by Chandler
+    //use new window instead of modal for Youku videos
+    //to provide support for HTTPS pages
+    if (content.indexOf('youku.com/embed') !== -1) {
+      var w = 860,
+        h = 524,
+        left = screen.width / 2 - w / 2,
+        top = screen.height / 2 - h / 2;
+      window.open(content, '', 'location=1,resizable=yes,top=' + top + ',left=' + left + ',width=' + w + ',height=' + h);
+      e.preventDefault();
+      return false;
+    }
+    //load modal window with content passed
+    $('<div id="modal-window" class="window">' + fullContent + '<a href="#" class="close">X</a></div>').appendTo('body');
+    $('#mask').css({
+      'width': maskWidth,
+      'height': maskHeight
+    });
+    $('#mask').show();
+    $('.window').css('top', ($(window).height() - $('.window').height()) / 2 + $(window).scrollTop() + 'px');
+    $('.window').css('left', ($(window).width() - $('.window').width()) / 2 + $(window).scrollLeft() + 'px');
+    $('.window').fadeIn();
+    e.preventDefault();
+    return false;
+  });
+  //if close button is clicked
+  $('body').on('click', '.window .close', function (e) {
+    //Cancel the link behavior
+    e.preventDefault();
+    $('#mask, .window').fadeOut();
+    $('.window iframe').remove();
+  });
+  //if mask is clicked
+  $('#mask').on('click', function (e) {
+    //Cancel the link behavior
+    e.preventDefault();
+    $('#mask, .window').hide();
+    $('.window iframe').remove();
+  });
+  // Change modal window size repsonsive
+  function checkWindowSize(e) {
+    $('.window').css('top', ($(window).height() - $('.window').height()) / 2 + $(window).scrollTop() + 'px');
+    $('.window').css('left', ($(window).width() - $('.window').width()) / 2 + $(window).scrollLeft() + 'px');
+    $('#mask').css({
+      'width': $(window).width(),
+      'height': $(document).height()
+    });
+  }
+  $(window).bind('resize', checkWindowSize);
 }(jQuery));
 
